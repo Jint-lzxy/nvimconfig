0597f99e7b0db761041d52662c366bd2f2776e58
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-05-16 23:57:48 +0800

    Update notify layout

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -212,19 +212,22 @@ end
 
 function config.notify()
 	local notify = require("notify")
+
 	notify.setup({
 		---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
-		stages = "slide",
+		stages = "fade_in_slide_out",
 		---@usage Function called when a new window is opened, use for changing win settings/config
 		on_open = nil,
 		---@usage Function called when a window is closed
 		on_close = nil,
 		---@usage timeout for notifications in ms, default 5000
-		timeout = 2000,
+		timeout = 1000,
+		-- User render fps value
+		fps = 120,
 		-- Render function for notifications. See notify-render()
 		render = "default",
 		---@usage highlight behind the window for stages that change opacity
-		background_colour = "Normal",
+		background_colour = "Search",
 		---@usage minimum width for notification windows
 		minimum_width = 50,
 		---@usage Icons for the different levels
