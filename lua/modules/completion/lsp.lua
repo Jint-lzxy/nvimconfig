6c0009c94e7543cbf20a27677351ff01bc7be45a
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-05-29 11:37:35 +0800

    Update clangd lsp settings.
    现在需要外部定义clang-tidy参数了

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -113,7 +113,6 @@ for _, server in ipairs(lsp_installer.get_installed_servers()) do
 				"--pch-storage=memory",
 				"--query-driver=/usr/bin/clang++,/usr/bin/**/clang-*,/bin/clang,/bin/clang++,/usr/bin/gcc,/usr/bin/g++",
 				"--clang-tidy",
-				"--clang-tidy-checks=*",
 				"--all-scopes-completion",
 				"--cross-file-rename",
 				"--completion-style=detailed",
