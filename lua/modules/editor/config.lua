392724efd983146557cf75625b693fd4046de679
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-08-12 18:55:43 +0800

    chore(toggleterm): Update doc.

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -186,6 +186,8 @@ function config.toggleterm()
 			},
 		},
 		on_open = function()
+			-- Prevent infinite calls from freezing neovim.
+			-- Only set these options specific to this terminal buffer.
 			vim.api.nvim_set_option_value("foldmethod", "manual", { scope = "local" })
 			vim.api.nvim_set_option_value("foldexpr", "0", { scope = "local" })
 		end,
