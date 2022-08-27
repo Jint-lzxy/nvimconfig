4b338275e1fa070ad33f55292dac73320fdfadef
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-08-27 17:54:53 +0800

    chore: Cleanup.

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -1,7 +1,6 @@
 local formatting = require("modules.completion.formatting")
 
 vim.cmd([[packadd lsp_signature.nvim]])
-vim.cmd([[packadd lspsaga.nvim]])
 vim.cmd([[packadd cmp-nvim-lsp]])
 vim.cmd([[packadd nvim-navic]])
 
