884fdf98f28199582e73441ffdee64317a34596f
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-08-27 17:54:14 +0800

    refactor(lspsaga): Use custom_kind instead for icons.

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -299,34 +299,41 @@ function config.lspsaga()
 	vim.api.nvim_set_hl(0, "LspFloatWinNormal", { bg = "#1E1E2E" })
 	vim.api.nvim_set_hl(0, "SagaShadow", { bg = "#1E1E2E" })
 
-	-- Set icons for sidebar.
-	local diagnostic_icons = {
-		Error = " ",
-		Warn = " ",
-		Info = " ",
-		Hint = " ",
-	}
-	for type, icon in pairs(diagnostic_icons) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl })
+	local function set_sidebar_icons()
+		-- Set icons for sidebar.
+		local diagnostic_icons = {
+			Error = " ",
+			Warn = " ",
+			Info = " ",
+			Hint = " ",
+		}
+		for type, icon in pairs(diagnostic_icons) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl })
+		end
 	end
 
-	local kind = require("lspsaga.lspkind")
-	kind[2][2] = " "
-	kind[4][2] = " "
-	kind[5][2] = "ﴯ "
-	kind[6][2] = " "
-	kind[7][2] = "ﰠ "
-	kind[8][2] = " "
-	kind[9][2] = " "
-	kind[10][2] = " "
-	kind[11][2] = " "
-	kind[12][2] = " "
-	kind[13][2] = " "
-	kind[15][2] = " "
-	kind[16][2] = " "
-	kind[23][2] = " "
-	kind[26][2] = " "
+	local function get_palette()
+		if vim.g.colors_name == "catppuccin" then
+			-- If the colorscheme is catppuccin then use the palette.
+			return require("catppuccin.palettes").get_palette()
+		else
+			-- Default behavior: return lspsaga's default palette.
+			local palette = require("lspsaga.lspkind").colors
+			palette.peach = palette.orange
+			palette.flamingo = palette.orange
+			palette.rosewater = palette.yellow
+			palette.mauve = palette.violet
+			palette.sapphire = palette.blue
+			palette.maroon = palette.orange
+
+			return palette
+		end
+	end
+
+	set_sidebar_icons()
+
+	local colors = get_palette()
 
 	require("lspsaga").init_lsp_saga({
 		border_style = "single",
@@ -383,6 +390,39 @@ function config.lspsaga()
 			jump_key = "o",
 			-- auto refresh when change buffer
 			auto_refresh = true,
+		},
+		custom_kind = {
+			File = { " ", colors.rosewater },
+			Module = { " ", colors.blue },
+			Namespace = { " ", colors.blue },
+			Package = { " ", colors.blue },
+			Class = { "ﴯ ", colors.yellow },
+			Method = { " ", colors.blue },
+			Property = { "ﰠ ", colors.teal },
+			Field = { " ", colors.teal },
+			Constructor = { " ", colors.sapphire },
+			Enum = { " ", colors.yellow },
+			Interface = { " ", colors.yellow },
+			Function = { " ", colors.blue },
+			Variable = { " ", colors.peach },
+			Constant = { " ", colors.peach },
+			String = { " ", colors.green },
+			Number = { " ", colors.peach },
+			Boolean = { " ", colors.peach },
+			Array = { " ", colors.peach },
+			Object = { " ", colors.yellow },
+			Key = { " ", colors.red },
+			Null = { "ﳠ ", colors.yellow },
+			EnumMember = { " ", colors.teal },
+			Struct = { " ", colors.yellow },
+			Event = { " ", colors.yellow },
+			Operator = { " ", colors.sky },
+			TypeParameter = { " ", colors.maroon },
+			-- ccls-specific icons.
+			TypeAlias = { " ", colors.green },
+			Parameter = { " ", colors.blue },
+			StaticMethod = { "ﴂ ", colors.peach },
+			Macro = { " ", colors.red },
 		},
 	})
 end
