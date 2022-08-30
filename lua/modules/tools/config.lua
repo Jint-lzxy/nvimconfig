2a3f9bdf886f17174e59959ecb6bbf41454fa32c
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-08-30 14:45:00 +0800

    feat(telescope): Update configs.

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -22,6 +22,14 @@ function config.telescope()
 
 	require("telescope").setup({
 		defaults = {
+			vimgrep_arguments = {
+				"rg",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+			},
 			initial_mode = "insert",
 			prompt_prefix = "   ",
 			selection_caret = " ",
@@ -31,18 +39,32 @@ function config.telescope()
 			borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 			layout_strategy = "horizontal",
 			path_display = { "absolute" },
+			selection_strategy = "reset",
+			sorting_strategy = "ascending",
+			color_devicons = true,
 			file_ignore_patterns = { ".git/", ".cache", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip" },
 			layout_config = {
-				prompt_position = "bottom",
 				horizontal = {
-					preview_width = 0.5,
+					prompt_position = "top",
+					preview_width = 0.55,
+					results_width = 0.8,
 				},
+				vertical = {
+					mirror = false,
+				},
+				width = 0.85,
+				height = 0.92,
+				preview_cutoff = 120,
 			},
 			file_previewer = require("telescope.previewers").vim_buffer_cat.new,
 			grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
 			qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 			file_sorter = require("telescope.sorters").get_fuzzy_file,
 			generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+			buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+			mappings = {
+				n = { ["q"] = require("telescope.actions").close },
+			},
 		},
 		extensions = {
 			fzf = {
