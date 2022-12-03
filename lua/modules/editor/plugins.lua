3fefbc00ea2e2f9b97f7c5a4d5e4ea7607ad27cc
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-12-03 10:01:44 +0800

    chore(lazyload): Update command list for several plugins

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -151,7 +151,7 @@ editor["abecodes/tabout.nvim"] = {
 }
 editor["sindrets/diffview.nvim"] = {
 	opt = true,
-	cmd = { "DiffviewOpen" },
+	cmd = { "DiffviewOpen", "DiffviewClose" },
 }
 editor["brglng/vim-im-select"] = {
 	opt = true,
