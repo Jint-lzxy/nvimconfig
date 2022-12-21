2b07e0073e28ceb9a1603fc64c55286b0ad39975
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2022-12-21 10:31:36 +0800

    chore(clangfmt[lsp]): Update format options

--- lua/modules/completion/efm/formatters/clangfmt.lua
+++ lua/modules/completion/efm/formatters/clangfmt.lua
@@ -15,7 +15,7 @@ return {
             BreakBeforeTernaryOperators: true, BreakConstructorInitializers: BeforeColon, \z
             BreakInheritanceList: BeforeColon, BreakStringLiterals: true, \z
             Cpp11BracedListStyle: true, EmptyLineAfterAccessModifier: Leave, \z
-            IncludeBlocks: Regroup, IndentPPDirectives: BeforeHash, \z
+            IncludeBlocks: Regroup, IndentPPDirectives: AfterHash, \z
             IndentWrappedFunctionNames: true, PointerAlignment: Left, \z
             ReferenceAlignment: Pointer, SeparateDefinitionBlocks: Always, \z
             SortIncludes: CaseSensitive, SortUsingDeclarations: true, \z
