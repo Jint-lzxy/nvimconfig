0a0794e072676625f6645ede7e36507823246735
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-02-07 11:22:47 +0800

    feat(keymap): Let cursor move in and out of terminal.

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -7,7 +7,6 @@ local map_cmd = bind.map_cmd
 local def_map = {
 	-- Vim map
 	["n|<S-Tab>"] = map_cr("normal za"):with_noremap():with_silent(),
-	["n|<C-x>k"] = map_cr("bdelete"):with_noremap():with_silent(),
 	["n|<C-s>"] = map_cu("write"):with_noremap():with_silent(),
 	["n|<C-S-s>"] = map_cmd("execute 'silent! write !sudo tee % >/dev/null' <bar> edit!"):with_silent():with_noremap(),
 	["n|Y"] = map_cmd("y$"),
@@ -19,6 +18,10 @@ local def_map = {
 	["n|<C-l>"] = map_cmd("<C-w>l"):with_noremap(),
 	["n|<C-j>"] = map_cmd("<C-w>j"):with_noremap(),
 	["n|<C-k>"] = map_cmd("<C-w>k"):with_noremap(),
+	["t|<C-h>"] = map_cmd("<Cmd>wincmd h<CR>"):with_silent():with_noremap(),
+	["t|<C-l>"] = map_cmd("<Cmd>wincmd l<CR>"):with_silent():with_noremap(),
+	["t|<C-j>"] = map_cmd("<Cmd>wincmd j<CR>"):with_silent():with_noremap(),
+	["t|<C-k>"] = map_cmd("<Cmd>wincmd k<CR>"):with_silent():with_noremap(),
 	["n|“"] = map_cr("vertical resize -5"):with_silent(),
 	["n|‘"] = map_cr("vertical resize +5"):with_silent(),
 	["n|…"] = map_cr("resize -2"):with_silent(),
@@ -26,6 +29,7 @@ local def_map = {
 	["n|<C-q>"] = map_cmd(":wq<CR>"),
 	["n|Œ"] = map_cmd(":q!<CR>"),
 	["n|<leader>o"] = map_cr("setlocal spell! spelllang=en_us"),
+	["x|<leader>p"] = map_cmd('"_dP'):with_silent():with_noremap(),
 	-- Insert
 	["i|<C-u>"] = map_cmd("<C-G>u<C-U>"):with_noremap(),
 	["i|<C-b>"] = map_cmd("<Left>"):with_noremap(),
