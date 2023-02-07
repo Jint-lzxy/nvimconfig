275e7966566f505d6aafb74fb526b1ef013dc156
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-02-07 11:20:57 +0800

    chore(clever-f): Fix search key direction

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -141,6 +141,7 @@ function config.clever_f()
 	vim.g.clever_f_mark_char_color = "CleverChar"
 	vim.g.clever_f_mark_direct_color = "CleverChar"
 	vim.g.clever_f_mark_direct = true
+	vim.g.clever_f_fix_key_direction = true
 	vim.g.clever_f_timeout_ms = 1500
 end
 
