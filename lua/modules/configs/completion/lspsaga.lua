f80243014de2c822be406afc6238ff0f5dd520a7
冷酔閑吟 <50296129+Jint-lzxy@users.noreply.github.com>
2023-04-17 19:40:09 +0800

    pref(lspsaga): disable `header` for floating windows

--- lua/modules/configs/completion/lspsaga.lua
+++ lua/modules/configs/completion/lspsaga.lua
@@ -118,6 +118,7 @@ return function()
 			frequency = 12,
 		},
 		ui = {
+			title = false,
 			border = "single", -- Can be single, double, rounded, solid, shadow.
 			winblend = 0,
 			actionfix = icons.ui.Spell,
