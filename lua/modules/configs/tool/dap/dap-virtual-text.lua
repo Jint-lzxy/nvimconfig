3ce8a4e6d88a71440260c21947d3e410eef2f0c7
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-06-15 16:55:44 +0800

    pref(dap-virt-text): highlight new variables as changed

--- lua/modules/configs/tool/dap/dap-virtual-text.lua
+++ lua/modules/configs/tool/dap/dap-virtual-text.lua
@@ -3,11 +3,12 @@ return function()
 		enabled = true,
 		enabled_commands = true,
 		highlight_changed_variables = true,
-		highlight_new_as_changed = false,
+		highlight_new_as_changed = true,
 		show_stop_reason = true,
 		commented = false,
 		only_first_definition = true,
 		all_references = false,
+		clear_on_continue = false,
 		-- filter_references_pattern = '<module',
 		-- experimental features:
 		virt_text_pos = "eol",
