3e3f8f9cdb8e6a3ffd53e4295995e219d9e1af73
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-24 11:07:44 +0800

    feat(vim-cmake): update list of options

--- lua/modules/configs/tool/cmake.lua
+++ lua/modules/configs/tool/cmake.lua
@@ -1,4 +1,16 @@
 return function()
+	vim.g.cmake_command = "cmake"
+	vim.g.cmake_test_command = "ctest"
 	vim.g.cmake_default_config = "Debug"
+	vim.g.cmake_build_dir_location = "./build/"
+	vim.g.cmake_console_size = 15
+	vim.g.cmake_console_echo_cmd = 1
+	vim.g.cmake_jump = 0
+	vim.g.cmake_jump_on_completion = 0
+	vim.g.cmake_jump_on_error = 1
+	vim.g.cmake_link_compile_commands = 0
+	vim.g.cmake_statusline = 0
+	vim.g.cmake_restore_state = 1
+	vim.g.cmake_reinit_on_dir_changed = 1
 	vim.g.cmake_root_markers = { ".git", ".svn", "src" }
 end
