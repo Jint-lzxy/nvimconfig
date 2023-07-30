242d033e2e2cd88c3d06e9470b6126577536a4ac
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-07-30 12:36:09 +0800

    feat(persisted): enable autosave

--- lua/modules/configs/editor/persisted.lua
+++ lua/modules/configs/editor/persisted.lua
@@ -3,10 +3,14 @@ return function()
 		save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"), -- directory where session files are saved
 		silent = false, -- silent nvim message when sourcing session file
 		use_git_branch = true, -- create session files based on the branch of the git enabled repository
-		autosave = false, -- automatically save session files when exiting Neovim
-		-- Set `lazy = false` in `plugins/editor.lua` to enable this
+		autosave = true, -- automatically save session files when exiting Neovim
+		should_autosave = function()
+			if vim.bo.filetype == "alpha" then
+				return false
+			end
+			return true
+		end,
 		autoload = false, -- automatically load the session for the cwd on Neovim startup
-		on_autoload_no_session = nil, -- function to run when `autoload = true` but there is no session to load
 		follow_cwd = true, -- change session file name to match current working directory if it changes
 		allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
 		ignored_dirs = nil, -- table of dirs that are ignored when auto-saving and auto-loading
