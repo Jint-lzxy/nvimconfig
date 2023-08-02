13c5cd5c3f5d29d2e1a1b298564cf2e358120fae
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-02 10:39:43 +0800

    feat(better-escape): add missing `kj` mapping

--- lua/modules/configs/editor/better-escape.lua
+++ lua/modules/configs/editor/better-escape.lua
@@ -1,6 +1,6 @@
 return function()
 	require("better_escape").setup({
-		mapping = { "jk", "jj" }, -- a table with mappings to use
+		mapping = { "jk", "kj", "jj" }, -- a table with mappings to use
 		timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
 		clear_empty_lines = false, -- clear line after escaping if there is only whitespace
 	})
