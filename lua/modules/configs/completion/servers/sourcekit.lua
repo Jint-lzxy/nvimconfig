a143b396c96c160770cdf377bbd112374f6a5871
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-03 13:51:40 +0800

    fix(sourcekit): update list of filetype

--- lua/modules/configs/completion/servers/sourcekit.lua
+++ lua/modules/configs/completion/servers/sourcekit.lua
@@ -1,5 +1,5 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/sourcekit.lua
 return {
 	cmd = { "sourcekit-lsp", "--completion-max-results=500" },
-	filetypes = { "swift", "objective-c", "objective-cpp" },
+	filetypes = { "swift", "objc", "objcpp" },
 }
