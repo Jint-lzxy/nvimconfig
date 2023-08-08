a2ae335e02f994c2dab88524db24c401549205ef
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-08 11:19:04 +0800

    feat: re-enable `<Tab>[i]` for tabout

--- lua/modules/configs/editor/tabout.lua
+++ lua/modules/configs/editor/tabout.lua
@@ -1,11 +1,11 @@
 return function()
 	require("tabout").setup({
-		tabkey = "", -- key to trigger tabout, set to an empty string to disable
-		backwards_tabkey = "", -- key to trigger backwards tabout, set to an empty string to disable
+		tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+		backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
 		act_as_tab = true, -- shift content if tab out is not possible
 		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
 		enable_backwards = true,
-		completion = true, -- if the tabkey is used in a completion pum
+		completion = false,
 		tabouts = {
 			{ open = "'", close = "'" },
 			{ open = '"', close = '"' },
