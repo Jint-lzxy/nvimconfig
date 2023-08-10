3715a166a0bc84e2131c5bc7e095bd01b805266e
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-10 17:53:39 +0800

    feat(clang_format): update formatter options

--- lua/modules/configs/completion/formatters/clang_format.lua
+++ lua/modules/configs/completion/formatters/clang_format.lua
@@ -3,20 +3,20 @@ return {
         BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: ForIndentation, \z
         NamespaceIndentation: All, AccessModifierOffset: -4, \z
         AlignArrayOfStructures: Right, AlignEscapedNewlines: Left, \z
-        AlignTrailingComments: true, AllowShortBlocksOnASingleLine: Always, \z
+        AlignTrailingComments: true, AllowShortBlocksOnASingleLine: Empty, \z
         AllowShortCaseLabelsOnASingleLine: true, AllowShortEnumsOnASingleLine: true, \z
         AllowShortFunctionsOnASingleLine: Inline, AllowShortLoopsOnASingleLine: false, \z
         BitFieldColonSpacing: Both, BreakBeforeBraces: Custom, BraceWrapping: {\z
         AfterCaseLabel: false, AfterClass: false, AfterEnum: false, AfterFunction: true, \z
-        AfterNamespace: true, AfterStruct: true, AfterUnion: true, AfterExternBlock: false, \z
+        AfterNamespace: true, AfterStruct: false, AfterUnion: true, AfterExternBlock: false, \z
         BeforeCatch: true, BeforeElse: false, BeforeLambdaBody: false, BeforeWhile: false, \z
-        SplitEmptyFunction: false, SplitEmptyRecord: false, SplitEmptyNamespace: false}, \z
-        FixNamespaceComments: true, ShortNamespaceLines: 1, ColumnLimit: 110, \z
+        SplitEmptyFunction: false, SplitEmptyRecord: false, SplitEmptyNamespace: true}, \z
+        FixNamespaceComments: true, ShortNamespaceLines: 1, ColumnLimit: 120, \z
         BreakBeforeTernaryOperators: true, BreakConstructorInitializers: BeforeColon, \z
         BreakInheritanceList: BeforeColon, BreakStringLiterals: true, \z
         Cpp11BracedListStyle: true, EmptyLineAfterAccessModifier: Leave, \z
         AlignConsecutiveMacros: Consecutive, IndentPPDirectives: AfterHash, \z
-        IncludeBlocks: Regroup, IndentWrappedFunctionNames: true, PointerAlignment: Left, \z
+        IncludeBlocks: Preserve, IndentWrappedFunctionNames: true, PointerAlignment: Left, \z
         ReferenceAlignment: Pointer, SeparateDefinitionBlocks: Always, \z
         AlignConsecutiveAssignments: Consecutive, SortUsingDeclarations: true, \z
         SortIncludes: CaseSensitive, SpaceAfterCStyleCast: true, SpaceBeforeCaseColon: false, \z
