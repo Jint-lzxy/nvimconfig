9fa1d77d0e9e3359b3ca281768f8761ee7c9320b
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-21 10:41:32 +0800

    feat(cmake): link compile commands after each run

--- lua/modules/configs/tool/cmake.lua
+++ lua/modules/configs/tool/cmake.lua
@@ -5,10 +5,7 @@ return function()
 	vim.g.cmake_build_dir_location = "./build/"
 	vim.g.cmake_console_size = 15
 	vim.g.cmake_console_echo_cmd = 1
-	vim.g.cmake_jump = 0
-	vim.g.cmake_jump_on_completion = 0
-	vim.g.cmake_jump_on_error = 1
-	vim.g.cmake_link_compile_commands = 0
+	vim.g.cmake_link_compile_commands = 1
 	vim.g.cmake_statusline = 0
 	vim.g.cmake_restore_state = 1
 	vim.g.cmake_reinit_on_dir_changed = 1
