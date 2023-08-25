3ba9f0081979e4db7275027129ff81a100b4d81d
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-08-26 00:39:20 +0900

    chore(vim-go): respect settings entry `external_browser`

--- lua/modules/configs/lang/vim-go.lua
+++ lua/modules/configs/lang/vim-go.lua
@@ -1,5 +1,7 @@
 return function()
+	vim.g.go_code_completion_enabled = 0
 	vim.g.go_doc_keywordprg_enabled = 0
 	vim.g.go_def_mapping_enabled = 0
-	vim.g.go_code_completion_enabled = 0
+	vim.g.go_play_browser_command = require("core.settings").external_browser .. " %URL%"
+	vim.g.go_version_warning = 0
 end
