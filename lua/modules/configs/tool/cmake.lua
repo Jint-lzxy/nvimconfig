66b788f6e1210653e6f627a584605c921367437b
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-11-15 20:02:35 +0800

    pref(cmake): disable auto-generate on save

--- lua/modules/configs/tool/cmake.lua
+++ lua/modules/configs/tool/cmake.lua
@@ -2,6 +2,7 @@ return function()
 	require("cmake-tools").setup({
 		cmake_command = "cmake", -- this is used to specify cmake command path
 		cmake_build_directory = "build/${variant:buildType}", -- this is used to specify generate directory for cmake, allows macro expansion
+		cmake_regenerate_on_save = false, -- do not auto (re)generate when saving CMakeLists.txt
 		cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
 		cmake_executor = {
 			name = "quickfix", -- name of the executor
