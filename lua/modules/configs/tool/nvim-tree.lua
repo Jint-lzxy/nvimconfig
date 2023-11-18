7bdf01d3c628b1c96ccccf5fc8a6d1c15437f517
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-11-18 10:32:01 +0800

    chore(nvim-tree): prune deleted options from setup func

--- lua/modules/configs/tool/nvim-tree.lua
+++ lua/modules/configs/tool/nvim-tree.lua
@@ -8,44 +8,32 @@ return function()
 
 	require("nvim-tree").setup({
 		auto_reload_on_write = true,
-		create_in_closed_folder = false,
 		disable_netrw = false,
 		hijack_cursor = true,
 		hijack_netrw = true,
 		hijack_unnamed_buffer_when_opening = true,
 		open_on_tab = false,
 		respect_buf_cwd = false,
+		prefer_startup_root = false,
 		sort_by = "name",
 		sync_root_with_cwd = true,
 		view = {
-			adaptive_size = false,
-			centralize_selection = false,
-			width = 30,
 			side = "left",
-			preserve_window_proportions = false,
 			number = false,
-			relativenumber = false,
 			signcolumn = "yes",
-			float = {
-				enable = false,
-				open_win_config = {
-					relative = "editor",
-					border = "rounded",
-					width = 30,
-					height = 30,
-					row = 1,
-					col = 1,
-				},
-			},
+			centralize_selection = false,
+			preserve_window_proportions = false,
+			float = { enable = false },
 		},
 		renderer = {
-			add_trailing = false,
-			group_empty = true,
-			highlight_git = true,
 			full_name = false,
+			group_empty = true,
+			add_trailing = false,
+			highlight_git = true,
+			symlink_destination = true,
+			root_folder_label = ":.:s?.*?/..?",
 			highlight_opened_files = "none",
 			special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "CMakeLists.txt" },
-			symlink_destination = true,
 			indent_markers = {
 				enable = true,
 				inline_arrows = true,
@@ -56,7 +44,6 @@ return function()
 					none = " ",
 				},
 			},
-			root_folder_label = ":.:s?.*?/..?",
 			icons = {
 				webdev_colors = true,
 				git_placement = "after",
@@ -96,19 +83,16 @@ return function()
 				},
 			},
 		},
-		hijack_directories = {
-			enable = true,
-			auto_open = true,
-		},
 		update_focused_file = {
 			enable = true,
 			update_root = true,
-			ignore_list = {},
 		},
 		filters = {
 			dotfiles = false,
+			no_buffer = false,
+			git_clean = false,
+			git_ignored = true,
 			custom = { ".DS_Store" },
-			exclude = {},
 		},
 		actions = {
 			use_system_clipboard = true,
@@ -149,31 +133,25 @@ return function()
 		},
 		git = {
 			enable = true,
-			ignore = false,
+			cygwin_support = false,
 			show_on_dirs = true,
-			timeout = 400,
-		},
-		trash = {
-			cmd = "trash -F",
-			require_confirm = true,
 		},
 		live_filter = {
 			prefix = "[FILTER]: ",
 			always_show_folders = true,
 		},
+		trash = {
+			cmd = "trash -F",
+		},
+		ui = {
+			confirm = {
+				remove = true,
+				trash = true,
+				default_yes = false,
+			},
+		},
 		log = {
 			enable = false,
-			truncate = false,
-			types = {
-				all = false,
-				config = false,
-				copy_paste = false,
-				dev = false,
-				diagnostics = false,
-				git = false,
-				profile = false,
-				watcher = false,
-			},
 		},
 	})
 end
