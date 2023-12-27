99c740a8fc2a2c1358ef0a213e77ecf5c09169fa
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-12-27 20:15:41 +0800

    chore: fix CI

--- lua/modules/configs/tool/cmake.lua
+++ lua/modules/configs/tool/cmake.lua
@@ -2,7 +2,7 @@ return function()
 	require("cmake-tools").setup({
 		cmake_command = "cmake", -- this is used to specify cmake command path
 		cmake_build_directory = "build/${variant:buildType}", -- this is used to specify generate directory for cmake, allows macro expansion
-		do_not_add_newline = false, -- do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter
+		do_not_add_newline = false, -- don't hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command
 		auto_close_when_success = true, -- auto-close the quickfix buffer if the execution is successful
 		cmake_regenerate_on_save = false, -- do not auto (re)generate when saving CMakeLists.txt
 		cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
