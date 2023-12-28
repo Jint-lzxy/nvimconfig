c3e05a9f3afebdddce2a429fdd3a0ed33c4a7d30
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2023-12-28 19:43:24 +0800

    feat(flash): disable multi line support

--- lua/modules/configs/editor/flash.lua
+++ lua/modules/configs/editor/flash.lua
@@ -27,7 +27,7 @@ return function()
 				-- show jump labels
 				jump_labels = false,
 				-- set to `false` to use the current line only
-				multi_line = true,
+				multi_line = false,
 				-- When using jump labels, don't use these keys
 				-- This allows using those keys directly after the motion
 				label = { exclude = "hjkliardc" },
