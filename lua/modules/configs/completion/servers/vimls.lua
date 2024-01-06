3d662c0766828df483366444a2e247b36ef58090
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-01-06 22:51:35 +0800

    feat(lsp): add config for `vimls`

---
+++ lua/modules/configs/completion/servers/vimls.lua
@@ -0,0 +1,11 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/vimls.lua
+return {
+	cmd = { "vim-language-server", "--stdio" },
+	filetypes = { "vim" },
+	single_file_support = true,
+	init_options = {
+		isNeovim = true,
+		iskeyword = "@,48-57,_,192-255",
+		diagnostic = { enable = true },
+	},
+}
