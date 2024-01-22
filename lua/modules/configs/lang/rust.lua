3eef0550ccefef6e1266ffe094b3546cde6c88f9
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-01-22 23:07:42 +0800

    fix(rust): follow upstream changes
    See mrcjkb/rustaceanvim@819ae2d

--- lua/modules/configs/lang/rust.lua
+++ lua/modules/configs/lang/rust.lua
@@ -1,6 +1,10 @@
 return function()
 	vim.g.rustaceanvim = {
 		-- Disable automatic DAP configuration to avoid conflicts with previous user configs
-		dap = { adapter = false, configuration = false },
+		dap = {
+			adapter = false,
+			configuration = false,
+			autoload_configurations = false,
+		},
 	}
 end
