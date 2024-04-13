8d7049eeb0011913ff016bba0d20c1e9739fe5a4
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-04-13 18:53:03 +0800

    chore(paint): use a better highlight group
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/ui/paint.lua
+++ lua/modules/configs/ui/paint.lua
@@ -5,15 +5,15 @@ return function()
 			{
 				-- filter can be a table of buffer options that should match,
 				-- or a function called with buf as param that should return true.
-				-- The example below will paint @something in comments with Constant
+				-- The example below will paint @something in comments with markdownCode
 				filter = { filetype = "lua" },
 				pattern = "%s*%-%-%-%s*(@%w+)",
-				hl = "MarkdownTodo",
+				hl = "markdownCode",
 			},
 			{
 				filter = { filetype = "python" },
 				pattern = "%s*([_%w]+:)",
-				hl = "MarkdownTodo",
+				hl = "markdownCode",
 			},
 		},
 	})
