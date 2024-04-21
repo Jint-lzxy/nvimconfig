e66898fcf9c808f8f53e06a926ee905199eabe23
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-04-21 22:04:27 +0800

    feat!: improve go development experience
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

---
+++ lua/modules/configs/lang/go.lua
@@ -0,0 +1,21 @@
+return function()
+	require("go").setup({
+		-- Disable these options to avoid conflicts with the global gopls config
+		icons = false,
+		diagnostic = false,
+		lsp_cfg = false,
+		lsp_gofumpt = false,
+		lsp_keymaps = false,
+		lsp_codelens = false,
+		lsp_document_formatting = false,
+		lsp_inlay_hints = { enable = false },
+		-- DAP-related settings are also turned off here for the same reason
+		dap_debug = false,
+		dap_debug_keymap = false,
+		textobjects = false,
+		-- Miscellaneous options to seamlessly integrate with other plugins
+		trouble = true,
+		luasnip = false,
+		run_in_floaterm = false,
+	})
+end
