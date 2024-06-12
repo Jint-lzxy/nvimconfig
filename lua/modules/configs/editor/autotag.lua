80014f528334dcc3ed047c3459b02cf87fcd7956
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-12 14:28:17 +0800

    feat(autotag): follow upstream changes
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/autotag.lua
+++ lua/modules/configs/editor/autotag.lua
@@ -1,12 +1,9 @@
 return function()
 	require("nvim-ts-autotag").setup({
-		filetypes = {
-			"html",
-			"javascript",
-			"javascriptreact",
-			"typescriptreact",
-			"vue",
-			"xml",
+		opts = {
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
 		},
 	})
 end
