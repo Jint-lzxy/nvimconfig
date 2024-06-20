ed98164725d37dd6e0ad27b0046f13d677930c74
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-20 13:14:38 +0800

    feat(paint): highlight Python doc strings as `Constant`
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/ui/paint.lua
+++ lua/modules/configs/ui/paint.lua
@@ -1,11 +1,7 @@
 return function()
 	require("paint").setup({
-		---type PaintHighlight[]
 		highlights = {
 			{
-				-- filter can be a table of buffer options that should match,
-				-- or a function called with buf as param that should return true.
-				-- The example below will paint @something in comments with markdownCode
 				filter = { filetype = "lua" },
 				pattern = "%s*%-%-%-%s*(@%w+)",
 				hl = "markdownCode",
@@ -13,7 +9,7 @@ return function()
 			{
 				filter = { filetype = "python" },
 				pattern = "%s*([_%w]+:)",
-				hl = "markdownCode",
+				hl = "Constant",
 			},
 		},
 	})
