2f133e399fa811c4c7a2546b1c83aca7ba8f0111
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-20 13:14:21 +0800

    fix(gitsigns): upstream breaking changes
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/ui/gitsigns.lua
+++ lua/modules/configs/ui/gitsigns.lua
@@ -1,45 +1,22 @@
 return function()
 	require("gitsigns").setup({
 		signs = {
-			add = {
-				hl = "GitSignsAdd",
-				text = "┃",
-				numhl = "GitSignsAddNr",
-				linehl = "GitSignsAddLn",
-			},
-			change = {
-				hl = "GitSignsChange",
-				text = "┃",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-			delete = {
-				hl = "GitSignsDelete",
-				text = "_",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "‾",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "~",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
-		watch_gitdir = { interval = 1000, follow_files = true },
-		current_line_blame = true,
-		current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
+		auto_attach = true,
+		signcolumn = true,
 		sign_priority = 6,
 		update_debounce = 100,
-		status_formatter = nil, -- Use default
 		word_diff = false,
+		current_line_blame = true,
 		diff_opts = { internal = true },
+		watch_gitdir = { follow_files = true },
+		current_line_blame_opts = { delay = 1000, virt_text = true, virtual_text_pos = "eol" },
 		on_attach = function(bufnr)
 			local actions = require("gitsigns.actions")
 			local bind = require("keymap.bind")
