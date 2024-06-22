6f4ff88df13dfa6571421a2361e7ace90e9cc8ec
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-22 11:48:28 +0800

    fix(keymap): should be `setlocal undofile!`
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -65,11 +65,11 @@ local core_map = {
 	["v|<"] = map_cmd("<gv"):with_silent(),
 	["v|>"] = map_cmd(">gv"):with_silent(),
 	-- Accoustomed Settings
-	["n|¨"] = map_cr("set undofile! undofile?"):with_noremap(),
 	["n|<Up>"] = map_cr("resize +1"):with_silent():with_noremap(),
 	["n|<Down>"] = map_cr("resize -1"):with_silent():with_noremap(),
 	["n|<Left>"] = map_cr("vertical resize +2"):with_silent():with_noremap(),
 	["n|<Right>"] = map_cr("vertical resize -2"):with_silent():with_noremap(),
+	["n|¨"] = map_cr("setlocal undofile! undofile?"):with_noremap(),
 }
 
 bind.nvim_load_mapping(core_map)
