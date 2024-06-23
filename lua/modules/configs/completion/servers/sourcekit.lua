1f28b6af55c30f0ddd2120174db64c3238444ccf
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-23 19:08:19 +0800

    feat(lsp): limit `sourcekit`'s maximum results to `150`
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/servers/sourcekit.lua
+++ lua/modules/configs/completion/servers/sourcekit.lua
@@ -1,5 +1,5 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/sourcekit.lua
 return {
-	cmd = { "sourcekit-lsp", "--completion-max-results=500" },
+	cmd = { "sourcekit-lsp", "--completion-max-results=150" },
 	filetypes = { "swift", "objc", "objcpp" },
 }
