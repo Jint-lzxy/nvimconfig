f1f45138bbb25df856565874ad2fc8e54462dc1f
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-06-24 11:38:28 +0800

    feat(cmp): swap the definition of `<CR>` and `<C-CR>`
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/cmp.lua
+++ lua/modules/configs/completion/cmp.lua
@@ -101,8 +101,8 @@ return function()
 		},
 		-- You can set mappings if you want
 		mapping = cmp.mapping.preset.insert({
-			["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
-			["<C-CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+			["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+			["<C-CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
 			["<C-p>"] = cmp.mapping.select_prev_item(),
 			["<C-n>"] = cmp.mapping.select_next_item(),
 			["<C-d>"] = cmp.mapping.scroll_docs(-4),
