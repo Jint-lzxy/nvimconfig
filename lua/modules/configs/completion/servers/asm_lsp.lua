031be784e5fe38e1a84dc7cd8c306b6710a467b9
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-03 16:45:10 +0800

    feat(asm_lsp): remove nonexistent filetypes
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/servers/asm_lsp.lua
+++ lua/modules/configs/completion/servers/asm_lsp.lua
@@ -1,4 +1,4 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/asm_lsp.lua
 return {
-	filetypes = { "asm", "nasm", "vmasm", "s", "S" },
+	filetypes = { "asm", "nasm", "vmasm" },
 }
