5500136b76f440966736d3f470106857d7bc010c
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-07 15:26:03 +0800

    feat(osv): cleanup
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/tool/dap/clients/osv.lua
+++ lua/modules/configs/tool/dap/clients/osv.lua
@@ -3,12 +3,12 @@ local dap = require("dap")
 
 dap.configurations.lua = {
 	{
-		type = "nvim",
+		type = "nlua",
 		request = "attach",
 		name = "Attach to a running Neovim instance",
 	},
 }
 
-dap.adapters.nvim = function(callback, config)
+dap.adapters.nlua = function(callback, config)
 	callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 7052 })
 end
