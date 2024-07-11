bd7f476ef89b6dca84ba56b57cdce404863a12d5
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-11 23:43:48 +0800

    feat(better-escape): remove default mappings and cleanup
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/better-escape.lua
+++ lua/modules/configs/editor/better-escape.lua
@@ -1,17 +1,12 @@
 return function()
-	local disabled = { j = { k = false, j = false } }
-
 	require("better_escape").setup({
 		timeout = vim.o.timeoutlen,
+		default_mappings = false,
 		mappings = {
 			i = {
-				j = { k = "<Esc>", j = "<Esc>" },
 				k = { j = "<Esc>" },
+				j = { k = "<Esc>", j = "<Esc>" },
 			},
-			c = disabled,
-			s = disabled,
-			v = disabled,
-			t = disabled,
 		},
 	})
 end
