7a5c4343c79d9085c5e635e9d79cd9a345ad97ce
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-12 22:30:30 +0800

    feat(lsp): include default capabilities
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/completion/lsp.lua
+++ lua/modules/configs/completion/lsp.lua
@@ -109,7 +109,11 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 })
 
 local opts = {
-	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	capabilities = vim.tbl_deep_extend(
+		"force",
+		vim.lsp.protocol.make_client_capabilities(),
+		require("cmp_nvim_lsp").default_capabilities()
+	),
 }
 ---A handler to setup all servers defined under `completion/servers/*.lua`
 ---@param lsp_name string
