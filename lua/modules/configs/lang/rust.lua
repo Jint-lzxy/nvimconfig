90a843b2029114aca89053ae64328da33cd7b1b2
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-12 22:31:11 +0800

    feat(rustaceanvim): use toggleterm as executor
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/lang/rust.lua
+++ lua/modules/configs/lang/rust.lua
@@ -6,7 +6,10 @@ return function()
 			configuration = false,
 			autoload_configurations = false,
 		},
+		tools = {
+			executor = require("rustaceanvim.executors").toggleterm,
+			reload_workspace_from_cargo_toml = true,
+		},
 		server = { standalone = true },
-		tools = { reload_workspace_from_cargo_toml = true },
 	}
 end
