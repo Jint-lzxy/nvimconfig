f0329fca8fd009f324396176c7efa2dfc0455a50
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-07-29 10:49:42 +0800

    feat(diffview): configure different layouts for individual views
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/diffview.lua
+++ lua/modules/configs/editor/diffview.lua
@@ -2,10 +2,30 @@ return function()
 	require("diffview").setup({
 		diff_binaries = false,
 		enhanced_diff_hl = false,
-		git_cmd = { "git" },
-		hg_cmd = { "hg" },
 		use_icons = true,
 		show_help_hints = true,
 		watch_index = true,
+		git_cmd = { "git" },
+		hg_cmd = { "hg" },
+		view = {
+			-- Config for changed files, and staged files in diff views.
+			default = {
+				layout = "diff2_horizontal",
+				disable_diagnostics = true,
+				winbar_info = false,
+			},
+			-- Config for conflicted files in diff views during a merge or rebase.
+			merge_tool = {
+				layout = "diff3_horizontal",
+				disable_diagnostics = true,
+				winbar_info = true,
+			},
+			-- Config for changed files in file history views.
+			file_history = {
+				layout = "diff2_horizontal",
+				disable_diagnostics = true,
+				winbar_info = false,
+			},
+		},
 	})
 end
