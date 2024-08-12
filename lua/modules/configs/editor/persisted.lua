4d847eefe560687c50b9a89692d093f62ef78888
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2024-08-12 12:26:02 +0800

    feat(persisted): update for recent code rewrite
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/editor/persisted.lua
+++ lua/modules/configs/editor/persisted.lua
@@ -1,17 +1,12 @@
 return function()
 	require("persisted").setup({
 		save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"),
-		silent = true,
-		autosave = true,
+		autostart = true,
 		autoload = false,
 		follow_cwd = true,
 		use_git_branch = true,
-		should_autosave = function()
-			if vim.bo.filetype == "alpha" then
-				return false
-			end
-			return true
+		should_save = function()
+			return vim.bo.filetype == "alpha" and false or true
 		end,
-		telescope = { reset_prompt_after_deletion = true },
 	})
 end
