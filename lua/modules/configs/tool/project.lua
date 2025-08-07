45c89ce365b37db0d558d0b02d5af10363ab380b
Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>
2025-08-08 00:36:42 +0800

    fix(project): enable automatic project detection
    Signed-off-by: Jint-lzxy <50296129+Jint-lzxy@users.noreply.github.com>

--- lua/modules/configs/tool/project.lua
+++ lua/modules/configs/tool/project.lua
@@ -1,6 +1,6 @@
 return function()
 	require("project_nvim").setup({
-		manual_mode = true,
+		manual_mode = false,
 		detection_methods = { "lsp", "pattern" },
 		patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
 		ignore_lsp = { "null-ls", "copilot" },
